fc71a8693b1a8a11e108b19fb68e379f5a5f4a1b
Pranjal <pranjalsingh10061997@gmail.com>
2024-09-05 16:15:51 +0530

    --

--- src/app/protectedRoutes/trips/[trip]/page.tsx
+++ src/app/protectedRoutes/trips/[trip]/page.tsx
@@ -2,7 +2,7 @@ import CustomTripDetailsComponent from "@/components/TripDetails/TripDetails"
 import { trips } from "@/data/data"
 
 function TripDetail({ params }: { params: { trip: string } }) {
-    const tripDetail = trips.find(t => t.key === +params.trip)
+    const tripDetail = trips.find(t => t.id === +params.trip)
     return (
         <div>
             {/* {JSON.stringify(tripDetail)} */}
