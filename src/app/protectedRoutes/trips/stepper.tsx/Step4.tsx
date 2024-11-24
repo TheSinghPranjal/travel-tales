a94cb21e49c739906e65aac772c7cf839f89fda4
Pranjal <pranjalsingh10061997@gmail.com>
2024-11-24 23:26:58 +0530

    task

--- src/app/protectedRoutes/trips/stepper.tsx/Step4.tsx
+++ src/app/protectedRoutes/trips/stepper.tsx/Step4.tsx
@@ -13,21 +13,35 @@ const Step4 = () => {
         secondaryEmail: ''
     });
 
+    const [billingData, setBillingData] = useState({
+        firstName: '',
+        lastName: '',
+        billingEmail: '',
+        pincode: '',
+        addressLine1: '',
+        addressLine2: ''
+    })
+
     useEffect(() => {
         // Load data from local storage when Step4 component mounts
         const savedData = localStorage.getItem('formData');
         if (savedData) {
             setFormData(JSON.parse(savedData));
         }
+        const savedBillingData = localStorage.getItem('billingData');
+        if (savedBillingData) {
+            setBillingData(JSON.parse(savedBillingData));
+        }
     }, []);
 
     return (
         <form className="step4-container">
-            <h3 className="section-title">Review Information</h3>
+            <h3 className="section-title">Review Personal Information</h3>
+
             <div className="form-columns">
                 <div className="form-column">
                     <div className="form-row">
-                        <label>First Name:</label>
+                        <label>First name:</label>
                         <TextField
                             fullWidth
                             variant="outlined"
@@ -38,7 +52,7 @@ const Step4 = () => {
                         />
                     </div>
                     <div className="form-row">
-                        <label>Phone Number:</label>
+                        <label>Phone number:</label>
                         <TextField
                             fullWidth
                             variant="outlined"
@@ -62,7 +76,7 @@ const Step4 = () => {
                 </div>
                 <div className="form-column">
                     <div className="form-row">
-                        <label>Last Name:</label>
+                        <label>Last name:</label>
                         <TextField
                             fullWidth
                             variant="outlined"
@@ -73,7 +87,7 @@ const Step4 = () => {
                         />
                     </div>
                     <div className="form-row">
-                        <label>Secondary Phone Number:</label>
+                        <label>Secondary phone number:</label>
                         <TextField
                             fullWidth
                             variant="outlined"
@@ -84,7 +98,7 @@ const Step4 = () => {
                         />
                     </div>
                     <div className="form-row">
-                        <label>Secondary Email:</label>
+                        <label>Secondary email:</label>
                         <TextField
                             fullWidth
                             variant="outlined"
@@ -96,6 +110,82 @@ const Step4 = () => {
                     </div>
                 </div>
             </div>
+            <hr></hr>
+            <h3 className="section-title">Review Billing Data</h3>
+
+            <div className="form-columns">
+                <div className="form-column">
+                    <div className="form-row">
+                        <label>First name:</label>
+                        <TextField
+                            fullWidth
+                            variant="outlined"
+                            value={billingData.firstName}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+                    </div>
+                    <div className="form-row">
+                        <label>Address 1:</label>
+                        <TextField
+                            fullWidth
+                            variant="outlined"
+                            value={billingData.addressLine1}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+                    </div>
+                    <div className="form-row">
+                        <label>Billing email:</label>
+                        <TextField
+                            fullWidth
+                            variant="outlined"
+                            value={billingData.billingEmail}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+                    </div>
+                </div>
+                <div className="form-column">
+                    <div className="form-row">
+                        <label>Last name:</label>
+                        <TextField
+                            fullWidth
+                            variant="outlined"
+                            value={billingData.lastName}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+                    </div>
+                    <div className="form-row">
+                        <label>Addess 2:</label>
+                        <TextField
+                            fullWidth
+                            variant="outlined"
+                            value={billingData.addressLine2}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+                    </div>
+                    <div className="form-row">
+                        <label>Pin code:</label>
+                        <TextField
+                            fullWidth
+                            variant="outlined"
+                            value={billingData.pincode}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
+
         </form>
     );
 };
